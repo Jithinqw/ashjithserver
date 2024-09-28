bc83438f020ca0888ad06e5434fcdbe64af45a3f
Jithinqw <zaccydiai7@gmail.com>
2024-09-27 23:24:48 -0600

    Intial commit

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+const config = require('./locale.json');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.json(config));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
