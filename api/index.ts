1fe53374ae526487b4bf098f558d56b2d658907e
Jithinqw <zaccydiai7@gmail.com>
2024-09-27 23:47:36 -0600

    Intial commit

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,27 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+const app = require('express')(),
+    cors = require("cors"),
+    compression = require("compression"),
+    helmet = require("helmet");
+
+app.use(app.json());
+app.use(cors());
+app.options("*", cors());
+app.use(helmet());
+app.use(compression());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy({setTo:"PHP 4.2.2"}));
 const config = require('./data/locale.json');
 
 app.get("/", (req, res) => res.json(config));
+
+app.use(function(err, req, res, next) {
+    return res.status(500).send({ error: err });
+});
+
+app.use("*", (req,res)=> {
+    res.status(404).json("The route you requested has not been found");
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
